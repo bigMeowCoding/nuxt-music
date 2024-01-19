a27bd4ac775cfb37523d3cc93d5989edc4cb00e8
zhouyijun <zhou495248579@hotmail.com>
2024-01-19 23:40:24 +0800

    feat:recommend 接口

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from "path";
+import proxy from "./middlewares/proxy";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  serverMiddleware: [proxy],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: `@import "${path.resolve(
-              __dirname,
-              "assets/styles/variable.scss"
+            __dirname,
+            "assets/styles/variable.scss"
           )}";
           @import "${path.resolve(__dirname, "assets/styles/mixin.scss")}";
           
@@ -17,5 +19,5 @@ export default defineNuxtConfig({
         },
       },
     },
-  }
-})
+  },
+});
